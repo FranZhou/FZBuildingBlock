6c20ccddeb1160add865134fe6c6a07ad8a963ed
zhoufan123 <zhoufan123@babytree-inc.com>
2019-02-21 17:06:33 +0800

    图片切圆角

---
+++ Example/FZBuildingBlock/Image/ImageCornerRadiusViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageCornerRadiusViewController.swift
+//  FZBuildingBlock_Example
+//
+//  Created by FranZhou on 2019/2/21.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ImageCornerRadiusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
